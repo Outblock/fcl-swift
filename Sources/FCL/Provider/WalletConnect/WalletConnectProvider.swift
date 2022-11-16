fc3d6314cbf786e2c2290b2c78d02c87b110e719
lmcmz <lmcmze@gmail.com>
2022-11-17 00:26:15 +1100

    Update encode method

--- Sources/FCL/Provider/WalletConnect/WalletConnectProvider.swift
+++ Sources/FCL/Provider/WalletConnect/WalletConnectProvider.swift
@@ -186,7 +186,7 @@ extension FCL {
                 throw Flow.FError.urlEmpty
             }
             var url = URL(string: endpoint)
-            if let encodedURI = uri?.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            if let encodedURI = uri?.absoluteString.addingPercentEncoding(withAllowedCharacters: .alphanumerics) {
                 url = URL(string: "\(endpoint)/wc?uri=\(encodedURI)")
             }
             
